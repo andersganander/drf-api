491677b3d9326c52c19c383374106f53d54ec4a4
Anders Ganander <anders.ganander@gmail.com>
2024-07-01 09:08:54 +0000

    Fix logout bug

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -34,8 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 CSRF_TRUSTED_ORIGINS = ['https://*.ws.codeinstitute-ide.net/']
 
